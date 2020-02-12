ce69ee8f7e03aa8502812cf0668f56d38126c100
ReinitiaIized <reinitialized@reinitialized.studio>
2020-02-12 13:42:13 -0600

    began rewriting Core to utilize OOP standards

--- Core/init.lua
+++ Core/init.lua
@@ -1,96 +1,33 @@
-local ModulesManager = {}
-do
-    local loadedModules = {}
-    local objectModule = {prototype = {}}
-    objectModule.__index = objectModule.prototype
+local Core = {prototype = {}}
+Core.__index = Core.prototype
 
-    function ModulesManager:load(ModuleScript)
-        if loadedModules[ModuleScript.Name] then
-            return false, string.format(
-                "%s is already loaded into ModulesManager",
-                ModuleScript.Name
-            )
-        end
+local ModulesManager = {prototype = {}}
+ModulesManager.__index = ModulesManager.prototype
 
-        local newModuleObject = setmetatable(
-            {
-                _module = ModuleScript:clone(),
-                _api = {}
-            },
-            objectModule
-        )
-        
-        newModuleObject.started = newModuleObject._bindableStarted.Event
-        newModuleObject.stopping = newModuleObject._bindableStopping.Event
-        local controller = {require(newModuleObject._module:clone())}
-        newModuleObject._controller = {
-            start = controller[1],
-            stop = controller[2]
-        } 
-        loadedModules[ModuleScript.Name] = newModuleObject
-        return true
-    end
+local CachedModule = {prototype = {}}
+CachedModule.__index = CachedModule.prototype
 
-    function ModulesManager:unload(stringNameOfModule)
-        local unloadingModule = loadedModules[stringNameOfModule]
-        if not unloadingModule then
-            return false, string.format(
-                "%s is already unloaded from ModulesManager",
-                stringNameOfModule.Name
-            )
-        end
+function CachedModule.new(ModuleScript)
+    ModuleScript.Archivable = true
+    local self = setmetatable(
+        {   
+            -- clone the module so there's reduced risk of Modification
+            _safeModule = ModuleScript:Clone(),
+            _api = {},
+            _controller = {},
+            _events = {}
+        },
+        CachedModule
+    )
 
-        ModulesManager:stop(stringNameOfModule)
-        unloadingModule._bindableStarted:destroy()
-        unloadingModule._bindableStopping:destroy()
-        unloadingModule._controller.start = nil
-        unloadingModule._controller.stop = nil 
-    
-        return true
-    end
+    local requiredController = {require(self._safeModule)}
+    self._controller.start = requiredController[1]
+    self._controller.stop = requiredController[2]
 
-    function ModulesManager:start(stringNameOfModule, ...)
-        local moduleData = loadedModules[stringNameOfModule]
-        if not moduleData then
-            return false, string.format(
-                "%s is not a loaded Module",
-                stringNameOfModule
-            )
-        end
+    self._events.started = Instance.new("BindableEvent", self._safeModule)
+    self._events.stopping = Instance.new("BindableEvent", self._safeModule)
+    self.Started = self._events.started.Event
+    self.Stopping = self._events.stopping.Event
 
-        moduleData._thread = coroutine.create(moduleData._controller.start)
-        local response = {coroutine.resume(moduleData._thread, moduleData._api, ...)}
-        if response[1] then
-            moduleData._bindableStarted:fire(moduleData._api)
-        end
-        return unpack(response)
-    end
-
-    function ModulesManager:stop(stringNameOfModule, ...)
-        local moduleData = loadedModules[stringNameOfModule]
-        if not moduleData then
-            return false, string.format(
-                "%s is not a loaded Module",
-                stringNameOfModule
-            )
-        end
-
-        moduleData._bindableStopping:fire()
-        return coroutine.resume(
-            coroutine.create(
-                function()
-                    local success, failureReason = pcall(moduleData._controller.stop)
-                    for key, value in next, moduleData._api do
-                        moduleData._api[key] = nil
-                    end
-                    return success, failureReason
-                end
-            )
-        )
-    end
+    return self
 end
-
-local Core = {
-    ModulesManager = ModulesManager
-}
-return Core
