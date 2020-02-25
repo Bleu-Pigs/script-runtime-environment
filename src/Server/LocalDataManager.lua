405f09c0e4bccd8e98f61ab371a44cbda6b6fda9
ReinitiaIized <reinitialized@reinitialized.studio>
2020-02-25 16:01:27 -0600

    rewrote LocalDataManager

--- src/Server/LocalDataManager.lua
+++ src/Server/LocalDataManager.lua
@@ -1,232 +1,171 @@
-local Core = require(script.Parent:WaitForChild("Core"))
-local Create = Core.Utilities.Create
+--[[
+
+- Adaptable soft limit based on numOfPlayers with hard limit of 20
+- Automatic retries up to set limit
+- Data buffer to reduce impact on request budget
+
+]]
+
+local Core = require(script.Parent.Core)
 local Get = Core.Utilities.Get
-local Promise = Core.Promise
+local Create = Core.Utilities.Create
 local t = Core.t
+local Promise = Core.Promise
 
 local DataStoreService = Get("DataStoreService")
 
-local DATASTORE_SECURITY_KEY = "some random key here"
-local REQUEST_TYPES = {
-    GET = newproxy(),
-    SET = newproxy(),
-    UPDATE = newproxy(),
-    ONUPDATE = newproxy()
+local REQUESTS = {
+    Get = newproxy(),
+    Set = newproxy(),
+    Update = newproxy(),
+    OnUpdate = newproxy()
 }
+local DATASTORE_SCOPE = "insert some security key here"
 
-local QueuedRequest = {}
 
-local queuedRequests = {}
+local ManagedLocalData = {prototype = {}}
+ManagedLocalData.__index = ManagedLocalData.prototype
+local activelyManagedLocalData = {}
 
-function QueuedRequest.new(DataStoreName, RequestType, Index, Value)
+function ManagedLocalData.new(name)
     assert(
-        t.string(DataStoreName),
+        t.string(name),
         string.format(
             "bad argument #1 (expecting string, got %s)",
-            typeof(DataStoreName)
-        )
-    )
-    assert(
-        t.literal(
-            REQUEST_TYPES.GET,
-            REQUEST_TYPES.SET,
-            REQUEST_TYPES.UPDATE,
-            REQUEST_TYPES.ONUPDATE
-        )(RequestType),
-        string.format(
-            "bad argument #2 (expecting REQUEST_TYPES Enum, got %s)",
-            typeof(RequestType)
+            typeof(name)
         )
     )
 
-    local request = {
-        DataStoreName,
-        RequestType,
-        Index,
-        Value,
-        Create(
-            "BindableEvent",
-            {
-                Parent = script
-            }
-        ),
-        0
-    }
+    local self = setmetatable(
+        {
+            _dataStoreName = name,
+            _dataBuffer = {},
+            _dataUpdateCallbacks = {}
+        },
+        ManagedLocalData
+    )
 
+    activelyManagedLocalData[name] = self
+    return self
+end
+
+function ManagedLocalData.prototype:Get(index)
     return Promise.async(
-        function(resolve, reject, onCancel)
-            resolve(request[5].Event:Wait())
+        function(resolve, reject)
+            local value = self._dataBuffer[index]
+            if t.table(value) then
+                return value[1]
+            end
+
+            -- data may not be cached, let's attempt to get it
+            local response = {
+                pcall(
+                    self._dataStore.GetAsync,
+                    self._dataStore,
+                    index
+                )
+            }
+
+            if response[1] then
+                self._dataBuffer[index] = {
+                    value,
+                    os.time(),
+                    true
+                }
+                resolve(value)
+            else
+                for i = 1, 10 do
+                    wait(1)
+                    response = {
+                        pcall(
+                            self._dataStore.GetAsync,
+                            self._dataStore,
+                            index
+                        )
+                    }
+
+                    if response[1] then
+                        self._dataBuffer[index] = {
+                            value,
+                            os.time(),
+                            true
+                        }
+                        return resolve(value)
+                    end
+                end
+
+                reject(response[2])
+            end
         end
     )
 end
 
-local ManagedLocalData = {prototype = {}}
-ManagedLocalData.__index = ManagedLocalData.prototype
+function ManagedLocalData.prototype:Set(index, value)
+    self._dataBuffer[index] = {
+        value,
+        os.time(),
+        false
+    }
 
-local managedDataStores = {}
-
-do
-    function ManagedLocalData.new(Name)
-        local self = setmetatable(
-            {
-                _dataStoreName = Name,
-                _dataBuffer = {}
-            },
-            ManagedLocalData
-        )
-        managedDataStores[Name] = self
-
-        return self
-    end
-
-    function ManagedLocalData.prototype:GetAsync(Index)
-        return QueuedRequest.new(self._dataStoreName, REQUEST_TYPES.GET, Index)
-    end
-
-    function ManagedLocalData.prototype:SetAsync(Index, Value)
-        return QueuedRequest.new(self._dataStoreName, REQUEST_TYPES.GET, Index, Value)
-    end
-
-    function ManagedLocalData.prototype:UpdateAsync(Index, Callback)ert(
-            t.callback(Callback),
-            string.format(
-                "bad argument #2 (expecting function, got %s)",
-                typeof(Callback)
-            )
-        )
-
-        return QueuedRequest.new(self._dataStoreName, REQUEST_TYPES.GET, Index, Callback)
-    end
-
-    function ManagedLocalData.prototype:OnUpdate(Index, Callback)
-        assert(
-            t.callback(Callback),
-            string.format(
-                "bad argument #2 (expecting function, got %s)",
-                typeof(Callback)
-            )
-        )
-
-        return QueuedRequest.new(self._dataStoreName, self._dataStoreNameBackup, REQUEST_TYPES.GET, Index, Callback)
-    end
+    return value
 end
 
 spawn(
     function()
-        -- TODO: implement OnClose handler logic
-        local request, response
-        local dataStore, dataStoreBackup
-        local timeSinceLastYield = os.time()
         while true do
-            if #queuedRequests > 0 then
-                request = table.remove(queuedRequests, 1)
-                print("processing request", request)
+            for _, ManagedLocalData in pairs(activelyManagedLocalData) do
+                for index, value in pairs(ManagedLocalData._dataBuffer) do
+                    if not value[3] and value[2] > (os.time() - 10) then
+                        -- data needs to be pushed to DataStore
+                        local dataStore = DataStoreService:GetDataStore(
+                            self._dataStoreName .."??".. index,
+                            DATASTORE_SCOPE
+                        )
+                        local backupDataStore = DataStoreService:GetOrderedDataStore(
+                            self._dataStoreName .."??".. index .."!!BACKUP",
+                            DATASTORE_SCOPE
+                        )
 
-                if request[6] > 3 then
-                    request[5]:Fire("request was unable to process after three attempts")
-                else
-                    dataStore = DataStoreService:GetDataStore(request[1], DATASTORE_SECURITY_KEY)
-
-                    if request[2] == REQUEST_TYPES.GET then
-                        response = {
-                            pcall(
-                                function()
-                                    local response = dataStore:GetAsync(request[3])
-                                    if t.table(response) then
-                                        return response[2]
-                                    end
+                        Promise.async(
+                            function(resolve)
+                                resolve(backupDataStore:GetOrderdAsync(false, 1):GetCurrentPage()[1])
+                            end
+                        ):andThen(
+                            function(mostRecentEntry)
+                                local timesSaved = 1
+                                if t.table(mostRecentEntry) then
+                                    timesSaved = mostRecentEntry[1] + 1
                                 end
-                            )
-                        }
-                    elseif request[2] == REQUEST_TYPES.SET then
-                        response = {
-                            pcall(
-                                function()
-                                    dataStore:UpdateAsync(
-                                        request[3],
-                                        function(oldData)
-                                            local timesChanged = 1
-                                            dataStoreBackup = DataStoreService:GetOrderedDataStore(
-                                                request[1] .."_".. tostring(request[3]) .."_Backup"
-                                            )
 
-                                            if t.table(oldData) and t.number(oldData[2]) then
-                                                timesChanged = oldData[2] + 1
-                                                dataStoreBackup:SetAsync(timesChanged, oldData)
-                                            end
+                                local timeSavedAt = os.time()
 
-                                            return {
-                                                request[4],
-                                                timesChanged
-                                            }
-                                        end
-                                    )
-
-                                    return request[4]
-                                end
-                            )
-                        }
-                    elseif request[2] == REQUEST_TYPES.UPDATE then
-                        response = {
-                            pcall(
-                                function()
-                                    return dataStore:UpdateAsync(
-                                        request[3],
-                                        function(oldData)
-                                            local timesChanged = 1
-                                            dataStoreBackup = DataStoreService:GetOrderedDataStore(
-                                                request[1] .."_".. tostring(request[3]) .."_Backup"
-                                            )
-
-                                            if t.table(oldData) and t.number(oldData[2]) then
-                                                timesChanged = oldData[2] + 1
-                                                dataStoreBackup:SetAsync(timesChanged, oldData)
-                                            end
-
-                                            return {
-                                                request[4](oldData),
-                                                timesChanged
-                                            }
-                                        end
-                                    )
-                                end
-                            )
-                        }
-                    elseif request[2] == REQUEST_TYPES.ONUPDATE then
-                        response = {pcall(request[1].OnUpdate, request[1], request[3], request[4])}
-                    end
-
-                    if not response[1] then
-                        print(unpack(response))
-                        table.insert(queuedRequests, request)
-                    else
-                        request[5]:Fire(response[2])
+                                backupDataStore:SetAsync(
+                                    timesSaved,
+                                    timeSavedAt
+                                )
+                                dataStore:SetAsync(
+                                    timeSavedAt,
+                                    value[1]
+                                )
+                            end
+                        )
                     end
                 end
             end
 
-            wait(1)
+            wait(10)
         end
     end
 )
 
-local LocalDataManager = {} do
-    function LocalDataManager:Get(Name)
-        assert(self == LocalDataManager, "Expected ':' not '.' calling member function Get")
-        assert(
-            t.string(Name),
-            string.format(
-                "bad argument #1 (expecting string, got %s)",
-                typeof(Name)
-            )
-        )
+local LocalDataManager = {}
 
-        if managedDataStores[Name] then
-            return managedDataStores[Name]
-        end
-
-        return ManagedLocalData.new(Name)
+function LocalDataManager:Get(name)
+    if activelyManagedLocalData[name] then
+        return activelyManagedLocalData[name]
     end
+
+    return ManagedLocalData.new(name)
 end
 
 return LocalDataManager
